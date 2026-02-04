a06f0cb202feb8b7b72537dedb808223b5fc0db9
Iurii Beliaev <ib86@duke.edu>
2026-02-04 16:09:36 -0500

    Added CREDIT_ROLEs and softmax method

--- author_share_model.py
+++ author_share_model.py
@@ -1,12 +1,27 @@
-class AuthorShareModel:
-    def __init__(self):
-        pass
+import json
+import math
+from typing import Any, Dict, List, Mapping, Union
 
+CREDIT_ROLES = [
+    "Conceptualization",
+    "Methodology",
+    "Software",
+    "Validation",
+    "Formal Analysis",
+    "Investigation",
+    "Resources",
+    "Data Curation",
+    "Writing – Original Draft",
+    "Writing – Review & Editing",
+    "Visualization",
+    "Supervision",
+    "Project Administration",
+    "Funding Acquisition",
+]
+
+def softmax(logits: List[float]) -> List[float]:
     """
-    Predict the author share of a text.
-    Args:
-        input: JSON object with the following fields:
-            
+    Apply softmax to a list of logits.
     """
-    def predict(self, input):
-        ret
+    exp_logits = [math.exp(logit) for logit in logits]
+    return [exp_logit / sum(exp_logits) for exp_logit in exp_logits]
