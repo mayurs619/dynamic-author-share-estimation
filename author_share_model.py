d054612c32737b29894c926c56948ee03e0cf314
Iurii Beliaev <ib86@duke.edu>
2026-02-04 16:08:39 -0500

    Created the author_share_model.py

---
+++ author_share_model.py
@@ -0,0 +1,12 @@
+class AuthorShareModel:
+    def __init__(self):
+        pass
+
+    """
+    Predict the author share of a text.
+    Args:
+        input: JSON object with the following fields:
+            
+    """
+    def predict(self, input):
+        ret
